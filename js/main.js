914a6bfb4caf59dcd00f786babe49f79cf33d532
*Wesley <wesleydaybusi12@gmail.com>
2022-03-28 20:02:18 -0300

    SUBINDO A PAGE DO WESLEY

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const PAGE_LIST = document.getElementById('list');
 const PAGE_ADD = document.getElementById('add');
 const PAGE_LOGIN = document.getElementById('login');
 const PAGE_SETTINGS = document.getElementById('settings');
+const PAGE_WESLEY = document.getElementById('wesley');
 
+PAGE_WESLEY.innerHTML = wesley();
 PAGE_LIST.innerHTML = listVehicles();
 PAGE_ADD.innerHTML = addVehicle();
 PAGE_LOGIN.innerHTML = login();
