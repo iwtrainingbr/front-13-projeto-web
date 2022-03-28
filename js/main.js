ccb2abcba4a8ea71d3fec54251ec1315a0b9253e
titodelerinofilho <titodelerino@yahoo.com.br>
2022-03-28 20:24:07 -0300

    adicionei uma nova funcao

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const PAGE_LIST = document.getElementById('list');
 const PAGE_ADD = document.getElementById('add');
 const PAGE_LOGIN = document.getElementById('login');
 const PAGE_SETTINGS = document.getElementById('settings');
+const PAGE_GUI = document.getElementById('gui');
 
 PAGE_LIST.innerHTML = listVehicles();
 PAGE_ADD.innerHTML = addVehicle();
 PAGE_LOGIN.innerHTML = login();
 PAGE_SETTINGS.innerHTML = settings();
+PAGE_GUI.innerHTML = gui();
