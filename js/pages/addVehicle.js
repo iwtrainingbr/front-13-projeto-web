6803700b7ea82a8ea9516d70dc300fdcee95185b
Ronaldo Cysne <ronaldocysne@gmail.com>
2022-04-03 18:30:17 -0300

    Formulário Box com imagem

--- js/pages/addVehicle.js
+++ js/pages/addVehicle.js
@@ -1,48 +1,52 @@
 
-function saveVehicle() {
-  event.preventDefault();
+// function saveVehicle() {
+//   event.preventDefault();
 
-  let newVehicle = {
-    placa: document.getElementById('plate').value,
-    marca: document.getElementById('make').value,
-    modelo: document.getElementById('model').value,
-    ano_modelo: document.getElementById('year').value,
-    ano_fabricacao: document.getElementById('year').value,
-    cor: document.getElementById('color').value,
-  };
+//   let newVehicle = {
+//     placa: document.getElementById('plate').value,
+//     marca: document.getElementById('make').value,
+//     modelo: document.getElementById('model').value,
+//     ano_modelo: document.getElementById('year').value,
+//     ano_fabricacao: document.getElementById('year').value,
+//     cor: document.getElementById('color').value,
+//   };
 
-  fetch(URL_VEHICLES, {
-    method: 'POST',
-    body: JSON.stringify(newVehicle),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
+//   fetch(URL_VEHICLES, {
+//     method: 'POST',
+//     body: JSON.stringify(newVehicle),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   });
 
-  alert('Veiculo inserido');
-  location.href = "index.html";
-}
+//   alert('Veiculo inserido');
+//   location.href = "index.html";
+// }
 
 
 function addVehicle() {
   return `
     <form action="" onsubmit="saveVehicle()">
-      <label for="plate">Placa</label>
-      <input type="text" id="plate" placeholder="Placa" class="form-control mb-3">
+      <label for="customer_name" class="mt-3">Nome</label>
+      <input type="text" id="customer_name" placeholder="Nome" class="form-control mb-3">
 
-      <label for="make">Marca</label>
-      <input type="text" id="make" placeholder="Marca" class="form-control mb-3">
+      <label class="mt-3 for="customer_email">Email</label>
+      <input type="email" id="customer_email" placeholder="email@email.com" class="form-control mb-3">
 
-      <label for="model">Modelo</label>
-      <input type="text" id="model" placeholder="Modelo" class="form-control mb-3">
+      <label class="mt-3 for="customer_phone">Telefone</label>
+      <input type="text" id="customer_phone" placeholder="85 99999-9999" class="form-control mb-3">
 
-      <label for="year">Ano</label>
-      <input type="text" id="year" placeholder="Ano" class="form-control mb-3">
+      <label class="mt-3 for="customer_adress">Endereço</label>
+      <input type="text" id="customer_adress" placeholder="Ex: Rua / Travessa / Avenida " class="form-control mb-3">
 
-      <label for="color">Cor</label>
-      <input type="text" id="color" placeholder="Cor" class="form-control mb-3">
+      <label class="mt-3 for="customer_birth">Data de Nascimento</label>
+      <input type="text" id="customer_birth" placeholder="dd/mm/aaaa" class="form-control mb-3">
 
-      <button class="btn btn-primary">ENVIAR</button>
+      <label class="mt-3 for="customer_sex">Sexo</label>
+      <input type="text" id="customer_sex" placeholder="Digite seu gênero" class="form-control mb-3">
+      <div class="d-grid mt-5">
+        <button class="btn btn-primary">ENVIAR</button>
+      </div>
     </form>
   `;
 }
