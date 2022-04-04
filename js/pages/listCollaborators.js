5a7d03ae41598cef35ae1b7ba71b004e332c6093
Victor <victoradler10@gmail.com>
2022-04-04 19:36:26 -0300

    listar colaboradores

---
+++ js/pages/listCollaborators.js
@@ -0,0 +1,44 @@
+const LIST_COLLABORATORS = 'http://localhost:8000/collaborators';
+
+
+function listCollaborators () {
+    //method for search customers for API
+
+    fetch(LIST_COLLABORATORS , {method: 'GET'} )
+    .then (response => response.json())
+    .then (collaborators =>{
+        collaborators.map (col =>{
+            document.getElementById('table-collaborators').innerHTML +=`
+                <tr>
+                    <td>${col.name}</td>
+                    <td>${col.email}</td>
+                    <td>${col.phone}</td>
+                    <td>${col.gender}</td>
+                    <td>${col.birth}</td>
+                    <td>${col.address}</td>
+                    <td>${col.specialty}</td>
+                </tr>
+            `
+        });
+    });
+
+    return `
+      <table class="table table-striped table-dark">
+        <thead class="table-dark">
+            <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Gender</th>
+            <th>Birth</th>
+            <th>Address</th>
+            <th>Specialty</th>
+            </tr>
+        </thead>
+        <tbody id="table-collaborators">
+        
+        </tbody>
+      </table>
+    `;
+    
+}
